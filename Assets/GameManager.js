2822f39fff984adc2594372afc522407a2a3704c
Alter Lagos <alter.strife@gmail.com>
2014-04-30 17:25:54 -0400

    tutorial complete up to step 5

---
+++ Assets/GameManager.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+static var playerScore1 : int = 0;
+static var playerScore2 : int = 0;
+
+var theSkin : GUISkin;
+
+static function Score (wallName : String) {
+  switch(wallName){
+    case 'rightWall': playerScore1++; break;
+    case 'leftWall': playerScore2++; break;
+  }
+
+}
+
+function OnGUI () {
+  GUI.skin = theSkin;
+  GUI.Label (new Rect(Screen.width/2 - 150, 20, 100, 100), '' + playerScore1);
+  GUI.Label (new Rect(Screen.width/2 + 150, 20, 100, 100), '' + playerScore2);
+}
