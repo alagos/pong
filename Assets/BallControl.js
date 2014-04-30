1e3070fdd8ceb83ca40a3e33f88fefd54831921b
Alter Lagos <alter.strife@gmail.com>
2014-04-30 18:40:48 -0400

    steps 7 and 8 completed

--- Assets/BallControl.js
+++ Assets/BallControl.js
@@ -7,10 +7,18 @@ function Start () {
   GoBall();
 }
 
+function update () {
+  var xVel : int = rigidbody2D.velocity.x;
+  if (xVel < 18 && xVel > -18 && xVel ==! 0) {
+    rigidbody2D.velocity.x = xVel > 0 ? 20 : -20;
+  };
+}
+
 function OnCollisionEnter2D (colInfo : Collision2D) {
   if (colInfo.collider.tag == 'Player') {
-    Debug.Log('player:' + colInfo.collider.rigidbody2D.velocity.y + "-ball:" + rigidbody2D.velocity.y);
+    // Debug.Log('player:' + colInfo.collider.rigidbody2D.velocity.y + "-ball:" + rigidbody2D.velocity.y);
     rigidbody2D.velocity.y = rigidbody2D.velocity.y/2 + colInfo.collider.rigidbody2D.velocity.y/3;
+    audio.Play();
   };
 }
 
--- Assets/GameManager.js
+++ Assets/GameManager.js
@@ -5,6 +5,12 @@ static var playerScore2 : int = 0;
 
 var theSkin : GUISkin;
 
+var theBall : Transform;
+
+function Start () {
+  theBall = GameObject.FindGameObjectWithTag('Ball').transform;
+}
+
 static function Score (wallName : String) {
   switch(wallName){
     case 'rightWall': playerScore1++; break;
@@ -15,6 +21,11 @@ static function Score (wallName : String) {
 
 function OnGUI () {
   GUI.skin = theSkin;
-  GUI.Label (new Rect(Screen.width/2 - 150 - 12, 20, 100, 100), '' + playerScore1);
-  GUI.Label (new Rect(Screen.width/2 + 150 - 12, 20, 100, 100), '' + playerScore2);
+  GUI.Label (new Rect(Screen.width/2 - 150 - 18, 20, 100, 100), '' + playerScore1);
+  GUI.Label (new Rect(Screen.width/2 + 150 - 18, 20, 100, 100), '' + playerScore2);
+
+  if (GUI.Button (new Rect(Screen.width/2-121/2, 35, 121, 53), 'RESET')) {
+    playerScore1 = playerScore2 = 0;
+    theBall.gameObject.SendMessage('ResetBall');
+  };
 }
